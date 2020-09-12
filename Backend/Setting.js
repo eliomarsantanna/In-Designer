63872d39508f394accf703bcfabd61662eb3d969
Eliomar Sant'Anna <57043262+eliomarsantanna@users.noreply.github.com>
2020-09-12 16:24:51 -0300

    Update Setting.js

--- Backend/Setting.js
+++ Backend/Setting.js
@@ -15,8 +15,8 @@ SystemJS.config({
     'react-dom': 'react-dom@16.4.2/umd/react-dom.development.js',
     'prop-types': 'prop-types@15.6/prop-types.js',
     'classnames': 'classnames@2.2.6/index.js',
-    '@material-ui/core': '@material-ui/core@3.2.2/umd/material-ui.development.js',
-    '@material-ui/icons': '@material-ui/icons@3.0.1/index.js',
+    '@in-designer/core': '@in-designer/core@3.2.2/umd/in-designer.development.js',
+    '@in-designer/icons': '@in-designer/icons@3.0.1/index.js',
   },
   transpiler: 'plugin-babel'
 });
